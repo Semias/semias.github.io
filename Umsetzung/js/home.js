77403b90e082191a7da34352f5fb69b56f3350b9
unknown <stefan.gall08@gmail.com>
2020-09-18 13:41:05 +0200

    18.09.2020

--- Umsetzung/js/home.js
+++ Umsetzung/js/home.js
@@ -1,26 +1,72 @@
+window.addEventListener(
+  "load",
+  function () {
+    let resp = document.querySelector(".responsive__menu");
+
+    resp.addEventListener("click", function () {
+      resp.classList.toggle("active");
+    });
+
+    /*let datenschutz = document.querySelector(".footer__policy ul li");
+    let policy = document.querySelector(".policy__info");
+    let policyClose = document.querySelector(".policy__info_close");
+
+    datenschutz.addEventListener("click", () => {
+      policy.classList.add("open");
+    });
+
+    policyClose.addEventListener("click", () => {
+      policy.classList.remove("open");
+    });*/
+
+    let datenschutz = document.querySelectorAll(".footer__policy ul li");
+    let policy = document.querySelector(".policy__info");
+    let policyClose = document.querySelectorAll(".policy__info_close");
+
+    datenschutz.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.stopPropagation();
+        policy.classList.add("open");
+      });
+    });
+
+    policyClose.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.stopPropagation();
+        policy.classList.remove("open");
+      });
+    });
+
+  },
+  false
+);
+
+
 jQuery.noConflict();
 jQuery(document).ready(function ($) {
   $(document).ready(function () {
-
     /*Burger Menue Click Function-End*/
 
     $(function () {
       $(".footer__logo svg").click(function () {
-        $("html,body").animate({
-          scrollTop: 0
-        }, "slow");
+        $("html,body").animate(
+          {
+            scrollTop: 0,
+          },
+          "slow"
+        );
       });
     });
 
     // Footer Impressum open usw..
 
-    $(".footer__policy ul li").click(function () {
+    /*$(".footer__policy ul li").click(function () {
       $(".policy__info").addClass("open");
     });
 
     $(".policy__info_close").click(function () {
       $(".policy__info.open").removeClass("open");
-    });
+    });*/
 
     //footer animate to section with delay
 
@@ -34,28 +80,4 @@ jQuery(document).ready(function ($) {
       });
     });
   });
-});
-
-
-let resp = document.querySelector(".responsive__menu");
-
-resp.addEventListener("click", function () {
-  resp.classList.toggle("active");
-});
-
-
-
-let datenschutz = document.querySelectorAll {
-  '.footer__policy ul li'
-};
-let info = document.querySelectorAll {
-  '.policy__info'
-};
-
-datenschutz.addEventListener("click", function () {
-  datenschutz.classList.add('open')
-});
-
-datenschutz.addEventListener("click", function () {
-  datenschutz.classList.add('open')
 });
